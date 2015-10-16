3883438cd7402665afea5d1e321fe5a6fc8ebae3
usergood <andreas.soderberg.1985@gmail.com>
2015-10-16 18:51:57 +0200

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /minecraft
-exec /sbin/setuser minecraft java -Xmx6024M -Xms1024M -jar minecraft_server.jar 2>&1 | logger
+exec /sbin/setuser minecraft java $MINECRAFT_OPTS -jar $MINECRAFT_STARTUP_JAR 2>&1 | logger
